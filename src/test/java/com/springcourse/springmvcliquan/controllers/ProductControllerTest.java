eff818df1bf18bcf60db8ba68489b1bbb8e34d36
quanlidavid <quanlidavid@gmail.com>
2018-08-13 08:32:35 +0800

    start mockito

---
+++ src/test/java/com/springcourse/springmvcliquan/controllers/ProductControllerTest.java
@@ -0,0 +1,97 @@
+package com.springcourse.springmvcliquan.controllers;
+
+import com.springcourse.springmvcliquan.domain.Product;
+import com.springcourse.springmvcliquan.services.ProductService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.mockito.Mockito.verifyZeroInteractions;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+public class ProductControllerTest {
+
+    @Mock //Mockito Mock object
+    private ProductService productService;
+
+    @InjectMocks //setups up controller, and injects mock objects into it
+    private ProductController productController;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        mockMvc = MockMvcBuilders.standaloneSetup(productController).build();
+    }
+
+    @Test
+    public void testList() throws Exception {
+        List<Product> products = new ArrayList<>();
+        products.add(new Product());
+        products.add(new Product());
+
+        //specific Mockito interaction, tell stub to return list of product
+        when(productService.listAll()).thenReturn((List) products);
+
+        mockMvc.perform(get("/product/list"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("product/list"))
+                .andExpect(model().attribute("products", hasSize(2)));
+
+    }
+
+    @Test
+    public void testShow() throws Exception {
+        Integer id = 1;
+
+        //tell mockito stub to return new product for ID 1
+        when(productService.getById(id)).thenReturn(new Product());
+
+        mockMvc.perform(get("/product/show/1"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("product/show"))
+                .andExpect(model().attribute("product", instanceOf(Product.class)));
+    }
+
+    @Test
+    public void testEdit() throws Exception {
+        Integer id =1;
+        when(productService.getById(id)).thenReturn(new Product());
+
+        mockMvc.perform(get("/product/edit/1"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("product/productform"))
+                .andExpect(model().attribute("product", instanceOf(Product.class)));
+    }
+
+    @Test
+    public void testNewProduct() throws Exception {
+        //should not call service
+        verifyZeroInteractions(productService);
+
+        mockMvc.perform(get("/product/new"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("product/productform"))
+                .andExpect(model().attribute("product", instanceOf(Product.class)));
+    }
+
+    @Test
+    public void testSaveOrUpdate() {
+
+    }
+
+}
